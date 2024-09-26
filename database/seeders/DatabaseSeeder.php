26217938cc3d8758f41e0fcd9161bc197bdafbf1
Yeray Garcia <yeraygarciamartinez5@gmail.com>
2024-09-26 19:58:28 +0100

    A6. Controlador, modelo y factories hecho.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\CommunityLink;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::delete('delete from community_links');
+        CommunityLink::factory(50)->create(); 
         // User::factory(10)->create();
 
         User::factory()->create([
