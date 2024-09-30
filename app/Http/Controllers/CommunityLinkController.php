4f157d8f9b1fd3a3fb6193cd5b66e74cc69c96fd
Yeray Garcia <yeraygarciamartinez5@gmail.com>
2024-09-30 16:51:48 +0100

    A7.Paginación y relaciones entre tablas.

--- app/Http/Controllers/CommunityLinkController.php
+++ app/Http/Controllers/CommunityLinkController.php
@@ -10,9 +10,9 @@ class CommunityLinkController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        return view('dashboard');
+    public function index() {
+        $links = CommunityLink::paginate(25);
+        return view('dashboard', compact('links'));
     }
 
     /**
