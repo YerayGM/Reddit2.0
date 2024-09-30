ced5a8b603f2738e213d050021c6c821473b12ea
Yeray Garcia <yeraygarciamartinez5@gmail.com>
2024-09-30 17:02:47 +0100

    A7. Paginación y relaciones entre tablas (ARREGLADO).

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,14 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __@foreach ($links as $link)
+                    {{ __("You're logged in!") }}
+                    @foreach ($links as $link)
                         <li>{{$link->title}}</li>
-                    @endforeach
-                    {{$links->links() }}}}
+                        @endforeach
+                    {{$links->links()}}
                     <!--Lo que hace este código es recorrer una colección de enlaces ($links) y generar una lista de elementos HTML (<li>) para cada uno 
                     de ellos, mostrando el título de cada enlace. Al final, utiliza el método links() para generar los enlaces de paginación si la colección 
                     está paginada.-->
-                    
                     <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
                     <!--Este código en Blade (Laravel) muestra información adicional sobre un enlace, específicamente quién lo contribuyó y cuándo fue actualizado, en un formato amigable para el usuario.-->
                 </div>
